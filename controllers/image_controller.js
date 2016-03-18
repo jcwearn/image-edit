d6004b24510e57c1d76fba7ba6532f6a598a2f9f
Jackson Wearn <jacksonwearn@gmail.com>
2016-03-18 13:56:41 -0400

    lwip working.  Need to come up with a way to interact now

--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -1,8 +1,26 @@
-var lwip = require('lwip');
+var fs = require('fs'),
+    path = require('path'),
+    lwip = require('lwip');
 
 var ImageController = {
   getImage: function() {
-    debugger;
+    var imgPath = path.join(__dirname, '../wat.jpg');
+    lwip.open(imgPath, function(err, image) {
+      if (image) {
+        image.saturate(-1, function(err, image) {
+          if (image) {
+            image.toBuffer('jpg', function(err, buffer) {
+              if (err) console.log(err);
+              if (buffer) {
+                fs.writeFile('output.jpg', buffer, (err) => {
+                  if (err) console.log(err);
+                });
+              }
+            });
+          }
+        });
+      }      
+    });
   }
 };
 
