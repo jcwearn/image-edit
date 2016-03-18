88dec189f797df46f9cd0aa015a663c372f57a9c
Jackson Wearn <jacksonwearn@gmail.com>
2016-03-18 10:17:01 -0400

    Add /image and /upload routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var ImageController = require('../controllers/image_controller.js'),
+    UploadController = require('../controllers/upload_controller.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/image', ImageController.getImage);
+
+router.get('/upload', UploadController.upload);
 
 module.exports = router;
