79766de4306b28896f74547c94ad23d1f90973d2
Jackson Wearn <jacksonwearn@gmail.com>
2016-03-18 10:43:20 -0400

    Comment out server stuff for the moment

--- app.js
+++ app.js
@@ -54,11 +54,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+/*
 var http = require('http');
 var port = process.env.PORT || '3000';
 app.set('port', port);
 
 var server = http.createServer(app);
-server.listen(port);
+server.listen(port);*/
 
 module.exports = app;
